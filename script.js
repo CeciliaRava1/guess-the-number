d2e961a7b803bf3dc9ef46d58ee4a9bf50502acc
Cecilia <ravacecilia1@gmail.com>
2024-07-24 17:31:14 -0300

    feat: Create the logic to show messages to the user in screen - If player's input number is = number to guess, print 'You win! - If player's input number is less than number to guess, print 'Is less' - Else print 'is greater'

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-// Get two values to define the range of the guess
+// Set two values to define the range of the guess
 const min = 1;
 const max = 100;
 
 //Get the target number to guess between these two values
 function getNumberToGuess(min, max) {
     return Math.round(Math.random() * (max - min) + min);
-  }
-result = getNumberToGuess(min,max);
-console.log(result);
+}
+result = getNumberToGuess(min, max);
 
 // Receive the player's input number
 function guessTheNumber() {
-    let x = document.getElementById("number-input").value;
-    document.getElementById("user-message").innerHTML = x;
+    let playerInputNumber = document.getElementById("number-input").value;
+    document.getElementById("user-message").innerHTML = playerInputNumber;
+
+    // Define a message for the player
+    if (playerInputNumber == result) {
+        document.getElementById("user-message").innerHTML = 'You win!';
+    } else if (playerInputNumber < result) {
+        document.getElementById("user-message").innerHTML = 'The number is greater';
+    } else {
+        document.getElementById("user-message").innerHTML = 'The number is less';
+    }
 }
-guessTheNumber();
 
 
 
-// Define a message for the player
-//     if (playerInputNumber == result){
-//         console.log('You win!')
-//         const guessed = true;
-//     } else if(playerInputNumber > result){
-//         console.log('The number is greater')
-//     } else {
-//         console.log('The number is less')
-//     }
-// }
 
-  
+

4c3fcbaf89cb3d438b9ee85b169da0d3dd5134de
Cecilia <ravacecilia1@gmail.com>
2024-07-24 16:52:19 -0300

    feat: Create conditionals to determine the message for the player

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ result = getNumberToGuess(min,max);
 console.log(result);
 
 // Receive the player's input number
-
-// Define a message for the player
+const guessed = false;
+while(!guessed){
+    let playerInputNumber = 10;
+    // Define a message for the player
+    if (playerInputNumber == result){
+        console.log('You win!')
+        const guessed = true;
+    } else if(playerInputNumber > result){
+        console.log('The number is greater')
+    } else {
+        console.log('The number is less')
+    }
+}
 
   

17601a51fe435aba221fed5ccfade0074472cf3a
Cecilia <ravacecilia1@gmail.com>
2024-07-24 17:18:53 -0300

    feat: Implement logic in JavaScript to receive the player's input number

--- script.js
+++ script.js
@@ -10,18 +10,23 @@ result = getNumberToGuess(min,max);
 console.log(result);
 
 // Receive the player's input number
-const guessed = false;
-while(!guessed){
-    let playerInputNumber = 10;
-    // Define a message for the player
-    if (playerInputNumber == result){
-        console.log('You win!')
-        const guessed = true;
-    } else if(playerInputNumber > result){
-        console.log('The number is greater')
-    } else {
-        console.log('The number is less')
-    }
+function guessTheNumber() {
+    let x = document.getElementById("number-input").value;
+    document.getElementById("user-message").innerHTML = x;
 }
+guessTheNumber();
+
+
+
+// Define a message for the player
+//     if (playerInputNumber == result){
+//         console.log('You win!')
+//         const guessed = true;
+//     } else if(playerInputNumber > result){
+//         console.log('The number is greater')
+//     } else {
+//         console.log('The number is less')
+//     }
+// }
 
   

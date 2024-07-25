b5b3bfa46e7b122c68b5c7245f259dfe00ff9ce8
Cecilia <ravacecilia1@gmail.com>
2024-07-25 07:11:06 -0300

    feat: Add amount of tries

--- script.js
+++ script.js
@@ -2,25 +2,31 @@
 const min = 1;
 const max = 100;
 
+// Show messages to the player
+let message = document.getElementById("user-message");
+let tri = document.getElementById("tri");
+let tries = 0;
+
 //Get the target number to guess between these two values
 function getNumberToGuess(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 result = getNumberToGuess(min, max);
 
-// Receive the player's input number
+
 function guessTheNumber() {
+    // Receive the player's input number
     let playerInputNumber = document.getElementById("number-input").value;
     document.getElementById("user-message").innerHTML = playerInputNumber;
-    let message = document.getElementById("user-message")
-
-    // Define a message for the player
     if (playerInputNumber >= 1 && playerInputNumber <= 100) {
+        tries ++;
+        tri.textContent = tries;
         if (playerInputNumber == result) {
             message.textContent = 'You win!';
             message.style.color = '#87c97c';
             message.style.fontSize = '100px';
             document.getElementById("number-input").setAttribute("disabled", "true");
+            document.getElementById("check-button").setAttribute("disabled", "true");
             return;
 
         } else if (playerInputNumber < result) {
